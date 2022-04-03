116ce52a12c463eb7a5d91dce416929a9e49a11a
mishaDrigval <drigval.ma.11@gmail.com>
2022-04-03 21:36:19 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    return arr1.length === arr2.length && arr1.every((item,index) => item === arr2[index])
 }
+
+console.log(compareArrays([8, 1, 2], [8, 1, 2]))
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+    return arr.filter((arr) => arr > 0).filter((arr) => arr % 3 === 0).map((arr) => arr * 10)
 }
+
+console.log(advancedFilter([-1,6,-9,3,]))
