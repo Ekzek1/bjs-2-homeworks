702ef37d40d17f12b512229ae0d1f2debf1c92ef
mishaDrigval <drigval.ma.11@gmail.com>
2022-04-09 20:15:21 +0300

    object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,30 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
-
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
+}
+Student.prototype.addMark = function(mark){
+    if(this.marks === undefined){ 
+        this.marks = [mark]
+    } else{
+        this.marks.push(mark)
+    }
+}
+Student.prototype.addMarks = function(...marks){
+    this.marks.push(...marks)
+}
+Student.prototype.getAverage = function(){
+    let sum = 0
+    this.marks.forEach((mark) => sum += mark)
+    return this.getAverage = sum/this.marks.length 
+}
+Student.prototype.exclude = function(reason){
+    delete this.subject
+    delete this.marks
+    this.exclude = reason
 }
 
-// ваш код для остальных методов
+
