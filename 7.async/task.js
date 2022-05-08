f6a5034f51d5e909ac0783329b19d6e5f30607d6
Mishadrigval <drigval.ma.11@gmail.com>
2022-05-08 18:06:29 +0300

    ff

--- 7.async/task.js
+++ 7.async/task.js
@@ -14,9 +14,56 @@ class AlarmClock {
         }
         return this.alarmCollection.push({id: id,time: time,callback:callback})
     }
-}
 
-const t = new AlarmClock;
-console.log(t.addClock(1,12,13));
-console.log(t)
-//console.log(t.addClock())
+    removeClock(id){
+        let timerId = this.alarmCollection.filter(item => item.id === id);
+        timerId = this.alarmCollection.findIndex(item => item.id === id)
+        if(timerId === -1){
+            return false
+        }else{
+            this.alarmCollection.splice(timerId,1);
+            return true
+        }
+    }
+
+    getCurrentFormattedTime(){
+       return new Date().toLocaleTimeString('ru-Ru', {
+        hour: '2-digit',
+        minute: '2-digit'
+       })
+    }
+
+    start(){
+        function checkClock  (alarm) {
+            if(alarm.time === this.getCurrentFormattedTime()){
+                return alarm.callback()
+            }
+        }
+        
+        let checkClockBind = checkClock.bind(this);
+
+        if(!this.timerId){
+            this.timerId = setInterval(() => {
+                    this.alarmCollection.forEach(alarm => checkClockBind(alarm))
+            }, 1000);
+        }
+    }
+
+    stop(){
+        if(!this.timerId){
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    printAlarms(){
+        this.alarmCollection.forEach(item => {
+            console.log(`индефикатор ${item.id} время звонка ${item.time}`
+        )})
+    }
+
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = []
+    }
+}
